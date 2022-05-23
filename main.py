5c934f4aa3882a943b84d489ab8dccd41dd3ddd1
Alexis T <alexistan9712@gmail.com>
2022-05-23 14:06:54 +0200

    🐢 Add simple autoclick feature

---
+++ main.py
@@ -0,0 +1,29 @@
+import pyautogui as p
+import keyboard as k
+import time as t
+
+# PyAutoGUI parameters
+p.FAILSAFE = True
+p.PAUSE = 0
+
+while True :
+    t.sleep(0.1)
+
+    # Get position on 1
+    if k.is_pressed('1'): 
+        p.position()
+        print(p.position())
+
+    if k.is_pressed('3'):
+        while True : 
+            p.click(p.position(), interval=0, clicks=5)
+            t.sleep(0.001)
+            # Stop the current loop
+            if k.is_pressed('9'):
+                print("Exit")
+                break
+
+    # End the program on press 9
+    if k.is_pressed('9'):
+        print("Exit")
+        break
